9f1105900299303306ead93a8c4421275622ffe5
unknown <faisalmad68@gmail.com>
2021-07-29 12:35:49 -0700

    JPA v2

--- src/main/java/guru/springframework/spring5webapp/domain/Book.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Book.java
@@ -1,8 +1,21 @@
 package guru.springframework.spring5webapp.domain;
 
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
 public class Book {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     private String title;
     private String isbn;
+
+    @ManyToMany
+    @JoinTable(name ="author_book" , joinColumns = @JoinColumn(name = "book_id"),
+            inverseJoinColumns = @JoinColumn(name = "author_id"))
     private Set<Author> authors;
 
     public Book() {
@@ -13,7 +26,13 @@ public class Book {
         this.isbn = isbn;
         this.authors = authors;
     }
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getTitle() {
         return title;
